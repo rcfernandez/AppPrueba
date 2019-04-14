cdac4da19abc07c38f87ebf1ba4cf214e6dccc14
rober fernandez <rcfernandez@outlook.com.ar>
2019-04-13 22:28:15 -0300

    primer commit

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+import { Camera,CameraOptions } from "@ionic-native/camera/ngx"; // 01.importamos la camara
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+
+export class Tab3Page {
+
+  image: string;
+
+  constructor(private camera : Camera) {
+  }
+
+  public takePicture() {
+    
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     console.log (err);
+    });
+
+  }
+
+
+
+}
